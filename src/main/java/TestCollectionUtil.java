7ef9b0eef6367914422c0cd4ea6ed5eb079d1a3d
STgitH70809 <sontra70809@gmail.com>
2018-12-03 18:38:05 +0700

    modified:   target/classes/TestCollectionUtil.class

--- src/main/java/TestCollectionUtil.java
+++ src/main/java/TestCollectionUtil.java
@@ -3,85 +3,71 @@ import static org.junit.Assert.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 public class TestCollectionUtil {
     @Test
-    public void testGetOrEmpty1(){
-        List<String> ls = null;
-        assertTrue(CollectionUtil.getOrEmpty(ls).isEmpty());
+    public void GetOrEmpty_NullInput_EmptyList(){
+        assertTrue(CollectionUtil.getOrEmpty(null).isEmpty());
         // if input is null getOrEmpty return a Empty List
     }
 
     @Test
-    public void testGetOrEmpty2(){
-        List<String> ls = new ArrayList<String>();
-        ls.add("good");
-        ls.add("morning");
+    public void GetOrEmpty_NonEmptyStringList_InputValue(){
+        List<String> ls = List.of("good","morning");
         assertEquals(ls,CollectionUtil.getOrEmpty(ls));
         // if input is not null getOrEmpty return a list like input
     }
 
     @Test
-    public void testFist1(){
-        List<String> ls = new ArrayList<String>();
-        ls.add("good");
-        ls.add("morning");
+    public void GetFirst_NonEmptyStringList_FirstListItem(){
+        List<String> ls = List.of("good","morning");
         assertEquals("good",CollectionUtil.getFirst(ls).get());
-        // if list not empty getFirst return the first vaule of the list
+        // if list not empty getFirst return the first value of the list
     }
 
     @Test
-    public void testFist2(){
-        List<String> ls = null;
-        assertFalse(CollectionUtil.getFirst(ls).isPresent());
+    public void GetFirst_NullInput_NotPresentOptionalObject(){
+        assertFalse(CollectionUtil.getFirst(null).isPresent());
         // if list is getFirst return not present Optional object
     }
 
     @Test
-    public void testFist3(){
-        List<String> ls = new ArrayList<String>();;
+    public void GetFirst_EmptyList_NotPresentOptionalObject(){
+        List<String> ls = List.of();
         assertFalse(CollectionUtil.getFirst(ls).isPresent());
         // if list is empty return not present Optional object
     }
 
     @Test
-    public void testNullSafeStream1(){
-        List<String> ls = new ArrayList<String>();
-        ls.add("a");
-        ls.add("b");
+    public void NullSafeStream_ListWithSomeNullValue_ListWithoutNullValue(){
+        List<String> ls = new ArrayList<String>(List.of("a","b"));
         ls.add(null);
         ls.add("c");
-        List<String> exp = new ArrayList<String>();
-        exp.add("a");
-        exp.add("b");
-        exp.add("c");
-        assertEquals(exp,CollectionUtil.nullSafeStream(ls).collect(Collectors.toList()));
+        List<String> exc = List.of("a","b","c");
+        assertEquals(exc,CollectionUtil.nullSafeStream(ls).collect(Collectors.toList()));
     }
     @Test
-    public void testNullSafeStream2(){
-        List<String> ls = new ArrayList<String>();
+    public void NullSafeStream_EmptyList_EmptyList(){
+        List<String> ls = List.of();
         assertTrue(CollectionUtil.nullSafeStream(ls).collect(Collectors.toList()).isEmpty());
     }
 
     @Test
-    public void testNullSafeStream3(){
+    public void NullSafeStream_ListContainOnlyNullValue_EmptyList(){
         List<String> ls = new ArrayList<String>();
         ls.add(null);
         assertTrue(CollectionUtil.nullSafeStream(ls).collect(Collectors.toList()).isEmpty());
     }
 
     @Test
-    public void testGetRandom1(){
-        Collection<String> ls = new ArrayList<String>();
-        ls.add("good");
-        ls.add("morning");
+    public void GetRandom_NotEmptyStringList_NotNullValue(){
+        Collection<String> ls = List.of("good","morning");
         assertTrue(CollectionUtil.getRandom(ls).isPresent());
         // if list is not empty return value will be Present
     }
 
     @Test
-    public void testGetRandom2(){
-        Collection<String> ls = new ArrayList<String>();
+    public void GetRandom_EmptyList_ReturnValueIsNull(){
+        Collection<String> ls = List.of();
         assertFalse(CollectionUtil.getRandom(ls).isPresent());
         // if list is empty return value will be not Present
     }
