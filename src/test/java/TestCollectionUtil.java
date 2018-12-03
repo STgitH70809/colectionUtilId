6956140e5707907ccde6a6ae26af5f824f93cea6
STgitH70809 <sontra70809@gmail.com>
2018-12-03 20:35:42 +0700

    new file:   .idea/uiDesigner.xml 	deleted:    src/main/java/TestRunner.java 	renamed:    src/main/java/TestCollectionUtil.java -> src/test/java/TestCollectionUtil.java 	modified:   .idea/misc.xml 	modified:   .idea/workspace.xml 	modified:   pom.xml 	modified:   target/classes/CollectionUtil.class 	deleted:    target/classes/TestCollectionUtil.class 	deleted:    target/classes/TestRunner.class 	.idea/vcs.xml 	target/classes/META-INF/ 	target/test-classes/

--- src/test/java/TestCollectionUtil.java
+++ src/test/java/TestCollectionUtil.java
@@ -5,40 +5,36 @@ import java.util.*;
 import java.util.stream.Collectors;
 public class TestCollectionUtil {
     @Test
-    public void GetOrEmpty_NullInput_EmptyList(){
+    public void getOrEmpty_nullInput_emptyList(){
         assertTrue(CollectionUtil.getOrEmpty(null).isEmpty());
-        // if input is null getOrEmpty return a Empty List
     }
 
     @Test
-    public void GetOrEmpty_NonEmptyStringList_InputValue(){
+    public void getOrEmpty_nonEmptyStringList_inputValue(){
         List<String> ls = List.of("good","morning");
         assertEquals(ls,CollectionUtil.getOrEmpty(ls));
-        // if input is not null getOrEmpty return a list like input
     }
 
     @Test
-    public void GetFirst_NonEmptyStringList_FirstListItem(){
+    public void getFirst_nonEmptyStringList_firstListItem(){
         List<String> ls = List.of("good","morning");
         assertEquals("good",CollectionUtil.getFirst(ls).get());
-        // if list not empty getFirst return the first value of the list
     }
 
     @Test
-    public void GetFirst_NullInput_NotPresentOptionalObject(){
+    public void getFirst_nullInput_notPresentOptionalObject(){
         assertFalse(CollectionUtil.getFirst(null).isPresent());
-        // if list is getFirst return not present Optional object
     }
 
     @Test
-    public void GetFirst_EmptyList_NotPresentOptionalObject(){
+    public void getFirst_emptyList_notPresentOptionalObject(){
         List<String> ls = List.of();
         assertFalse(CollectionUtil.getFirst(ls).isPresent());
         // if list is empty return not present Optional object
     }
 
     @Test
-    public void NullSafeStream_ListWithSomeNullValue_ListWithoutNullValue(){
+    public void nullSafeStream_listWithSomeNullValue_listWithoutNullValue(){
         List<String> ls = new ArrayList<String>(List.of("a","b"));
         ls.add(null);
         ls.add("c");
@@ -46,29 +42,27 @@ public class TestCollectionUtil {
         assertEquals(exc,CollectionUtil.nullSafeStream(ls).collect(Collectors.toList()));
     }
     @Test
-    public void NullSafeStream_EmptyList_EmptyList(){
+    public void nullSafeStream_emptyList_emptyList(){
         List<String> ls = List.of();
         assertTrue(CollectionUtil.nullSafeStream(ls).collect(Collectors.toList()).isEmpty());
     }
 
     @Test
-    public void NullSafeStream_ListContainOnlyNullValue_EmptyList(){
+    public void nullSafeStream_listContainOnlyNullValue_emptyList(){
         List<String> ls = new ArrayList<String>();
         ls.add(null);
         assertTrue(CollectionUtil.nullSafeStream(ls).collect(Collectors.toList()).isEmpty());
     }
 
     @Test
-    public void GetRandom_NotEmptyStringList_NotNullValue(){
+    public void getRandom_notEmptyStringList_notNullValue(){
         Collection<String> ls = List.of("good","morning");
         assertTrue(CollectionUtil.getRandom(ls).isPresent());
-        // if list is not empty return value will be Present
     }
 
     @Test
-    public void GetRandom_EmptyList_ReturnValueIsNull(){
+    public void getRandom_emptyList_returnValueIsNull(){
         Collection<String> ls = List.of();
         assertFalse(CollectionUtil.getRandom(ls).isPresent());
-        // if list is empty return value will be not Present
     }
 }
